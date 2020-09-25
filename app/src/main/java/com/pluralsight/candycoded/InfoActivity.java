025bb56387ac9bb303f911dd6c9c4b48b190fc19
Kashack <kasumu.elias@gmail.com>
2020-09-25 21:09:34 +0100

    Phone Activity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -24,10 +24,6 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
-
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
     public void createMapIntent(View view){
         Uri geo = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, geo);
@@ -36,8 +32,11 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
+    public void createPhoneIntent(View view){
+        Uri dial = Uri.parse("tel:0123456789");
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(dial);
+        startActivity(dialIntent);
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+    }
 }
